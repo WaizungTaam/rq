f09a92627682babb7d2deb4ff27279b1fb4e1ae4
Waizung Taam <waizungtaam@gmail.com>
2020-02-22 10:51:33 +0800

    [z] Comment worker_registration.py

--- rq/worker_registration.py
+++ rq/worker_registration.py
@@ -1,17 +1,18 @@
 from .compat import as_text
 
 
-WORKERS_BY_QUEUE_KEY = 'rq:workers:%s'
-REDIS_WORKER_KEYS = 'rq:workers'
+WORKERS_BY_QUEUE_KEY = 'rq:workers:%s'  # [z]: The name for the set the contains worker keys for the queue
+REDIS_WORKER_KEYS = 'rq:workers'  # [z]: The name for the set that contains all the worker keys
 
 
 def register(worker, pipeline=None):
     """Store worker key in Redis so we can easily discover active workers."""
     connection = pipeline if pipeline is not None else worker.connection
-    connection.sadd(worker.redis_workers_keys, worker.key)
+    connection.sadd(worker.redis_workers_keys, worker.key)  # [z]: Add worker key to set `rq:workers`
     for name in worker.queue_names():
         redis_key = WORKERS_BY_QUEUE_KEY % name
         connection.sadd(redis_key, worker.key)
+    # [z]: For each queue of the worker, add worker key to set `rq:workers:<queue_name>`
 
 
 def unregister(worker, pipeline=None):
@@ -21,10 +22,11 @@ def unregister(worker, pipeline=None):
     else:
         connection = pipeline
 
-    connection.srem(worker.redis_workers_keys, worker.key)
+    connection.srem(worker.redis_workers_keys, worker.key)  # [z]: Remove worker key from set `rq:workers`
     for name in worker.queue_names():
         redis_key = WORKERS_BY_QUEUE_KEY % name
         connection.srem(redis_key, worker.key)
+    # [z]: For each queue of the worker, remove worker key from set `rq:workers:<queue_name>`
 
     if pipeline is None:
         connection.execute()
@@ -38,11 +40,14 @@ def get_keys(queue=None, connection=None):
     if queue:
         redis = queue.connection
         redis_key = WORKERS_BY_QUEUE_KEY % queue.name
+        # [z] If a queue is given, return all the keys from set `rq:workers:<queue_name>`
     else:
         redis = connection
         redis_key = REDIS_WORKER_KEYS
+        # [z] If queue is not given, return all the keys from set `rq:workers` (all the workers for all queues)
 
     return {as_text(key) for key in redis.smembers(redis_key)}
+    # [z] Return a set of worker keys
 
 
 def clean_worker_registry(queue):
@@ -60,8 +65,12 @@ def clean_worker_registry(queue):
         for i, key_exists in enumerate(results):
             if not key_exists:
                 invalid_keys.append(keys[i])
+        # [z]: It seems like if a worker key does not exist as a redis key, it is invalid.
+        # [z?]: Find out more about the usage of the worker key.
 
         if invalid_keys:
             pipeline.srem(WORKERS_BY_QUEUE_KEY % queue.name, *invalid_keys)
+            # [z]: Remove from set `zq:workers:<queue_name>`
             pipeline.srem(REDIS_WORKER_KEYS, *invalid_keys)
+            # [z]: Remove from set `zq:workers`
             pipeline.execute()
